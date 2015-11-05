ee725c3a8025d13ae743be594d1f3fe967da49de
evgeni-tsn <evgeni.tsv.nikolov@gmail.com>
2015-11-05 11:29:01 +0200

    test

---
+++ Min 3 Numbers_Решение на JavaScript.js
@@ -0,0 +1,10 @@
+function solve(arr) {
+	var numbers = arr.splice(1).map(Number);
+	
+	numbers.sort(function (a, b) { return a - b; });
+	var smallest3Numbers = numbers.slice(0, 3);
+	
+	for (var i in smallest3Numbers) {
+		console.log(smallest3Numbers[i]);
+	}
+}
